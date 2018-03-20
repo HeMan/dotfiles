8706d8b32989a956a0d31584c2824677660870cf
Jimmy Hedman <jimmy.hedman@southpole.se>
2018-03-20 14:23:15 +0100

    Initial checkin.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+
+set -o nounset                              # Treat unset variables as an error
+
+check_and_link()
+{
+	SRC="$1"
+	DST="$2"
+	if [ -e $DST -o -L $DST ]; then
+	       echo "$DST existed, doin nothing"
+       else
+	       echo "$DST didn't exist, linkin"
+	       ln -s $SRC $DST
+       fi
+}
+
+cd ~/dotfiles
+
+git submodule update --init
+
+check_and_link ~/dotfiles/vim ~/.vim
+check_and_link ~/dotfiles/vimrc ~/.vimrc
+check_and_link ~/dotfiles/gitconfig ~/.gitconfig
+check_and_link ~/dotfiles/gitignore_global ~/.gitignore_global
+check_and_link ~/dotfiles/zshrc ~/.zshrc
