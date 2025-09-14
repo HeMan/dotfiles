776b1e0b072d2c5652bfd9911f0052fa769799ac
Jimmy Hedman <jimmy@clienttest.linuxguru.se>
2025-09-14 06:32:00 +0000

    Add zplug

---
+++ dot_packages.zsh
@@ -0,0 +1,23 @@
+zplug "lib/functions", from:oh-my-zsh
+zplug "lib/async_prompt", from:oh-my-zsh
+zplug "lib/directories", from:oh-my-zsh
+zplug "lib/completion", from:oh-my-zsh
+zplug "lib/grep", from:oh-my-zsh
+zplug "lib/key-bindings", from:oh-my-zsh
+zplug "lib/termsupport", from:oh-my-zsh
+
+zplug "plugins/battery", from:oh-my-zsh
+zplug "plugins/dotenv", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/mosh", from:oh-my-zsh
+zplug "plugins/screen", from:oh-my-zsh
+zplug "plugins/virtualenv", from:oh-my-zsh
+zplug "plugins/virtualenvwrapper", from:oh-my-zsh
+zplug 'endaaman/lxd-completion-zsh'
+
+zplug "themes/amuse", from:oh-my-zsh, as:theme
+zplug "chrissicool/zsh-256color"
+zplug "lib/history", from:oh-my-zsh
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "plugins/ztodo", from:oh-my-zsh
+zplug "plugins/gpg-agent", from:oh-my-zsh
